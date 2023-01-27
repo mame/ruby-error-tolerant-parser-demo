7a3dfeb76476612850e7886594b3a20e9c89f6ad
Yusuke Endoh <mame@ruby-lang.org>
2023-01-27 14:20:58 +0900

    parcel build for GitHub Pages

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 https://mame.github.io/ruby-error-tolerant-parser-demo/
 
 ```
-npx parcel build src/index.html
+npx parcel build src/index.html --dist-dir docs --public-url https://mame.github.io/ruby-error-tolerant-parser-demo/
 ```
--- docs/azcli.fca2e97f.js
+++
@@ -1,9 +0,0 @@
-function e(e,t,n,o){Object.defineProperty(e,t,{get:n,set:o,enumerable:!0,configurable:!0})}("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire94c2.register("5nuQN",(function(t,n){e(t.exports,"conf",(()=>o)),e(t.exports,"language",(()=>i));
-/*!-----------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.31.1(337587859b1c171314b40503171188b6cea6a32a)
- * Released under the MIT license
- * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
- *-----------------------------------------------------------------------------*/
-var o={comments:{lineComment:"#"}},i={defaultToken:"keyword",ignoreCase:!0,tokenPostfix:".azcli",str:/[^#\s]/,tokenizer:{root:[{include:"@comment"},[/\s-+@str*\s*/,{cases:{"@eos":{token:"key.identifier",next:"@popall"},"@default":{token:"key.identifier",next:"@type"}}}],[/^-+@str*\s*/,{cases:{"@eos":{token:"key.identifier",next:"@popall"},"@default":{token:"key.identifier",next:"@type"}}}]],type:[{include:"@comment"},[/-+@str*\s*/,{cases:{"@eos":{token:"key.identifier",next:"@popall"},"@default":"key.identifier"}}],[/@str+\s*/,{cases:{"@eos":{token:"string",next:"@popall"},"@default":"string"}}]],comment:[[/#.*$/,{cases:{"@eos":{token:"comment",next:"@popall"}}}]]}}}));
-//# sourceMappingURL=azcli.fca2e97f.js.map
